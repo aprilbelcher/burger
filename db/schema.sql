ac634054d0f3b19f20b346266c56f57ea33fdb61
April Denise Belcher <aprildenise@Aprils-MacBook-Pro.local>
2017-12-09 14:10:51 -0800

    server, npm, schema and config added

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+	id INT (10) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR (100) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES
+("Beef Double Cheese Burger"),
+("Patty Melt Burger"),
+("Mushroom & Swiss Double Cheese Burger");
